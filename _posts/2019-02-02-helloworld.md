7641e456a5c93beeb564e4b8f9ec62c14694ed8c
东风 <zhendong.cai@qq.com>
2019-02-02 11:32:32 +0800

    first commit

---
+++ _posts/2019-02-02-helloworld.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "你好，世界"
+date: 2019-02-02 11:26:01
+category: web
+---
+<h2>{{ page.title }}</h2>
+<p>我的第一篇文章</p>
+<p>{{ page.date | date_to_string }}</p>
---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?13b2e6c2630f446987dede305669b632";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
